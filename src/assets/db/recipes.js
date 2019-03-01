d9d18f7085621fbaea976a40590c893f12f94ee9
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-03-01 23:01:26 +0100

    Updated recipes data structure

---
+++ src/assets/db/recipes.js
@@ -0,0 +1,10 @@
+const recipes = [
+  {
+    id: 1,
+    name: 'Arroz con pescado y almejas',
+    preparation: '1:20h',
+    ingredientes: ['Almejas, Pescado blanco, 2 tomates maduros, 3 dientes de ajo, 1 cebolla, 2 tomates secos, Un dedo grande de jengibre fresco, Raspas de pescado / Espinas de lenguado, Vino blanco, Pasta de Miso rojo, 1 cucharada de soja baja en sodio, 1/2 lima'],
+    otros: ['Gambas, Hinojo (para el caldo), Puerro (para el caldo), Apio (para el caldo), 1 zanahoria (para el caldo)'],
+    preparacion: ['Fumé de pescado (25min) con ajo, cebolla, miso, raspas de pescado y ñoras, Sofreir los tomates maduros hasta que pierdan el 80% de agua, Sofreir el arroz con los tomates, Añadir vaso de vino blanco a fuego fuerte para que evapore, Ir echando el fumé poco a poco mientras se consume, Mover el arroz como mucho 3 veces para que no se rompa a lo largo del tiempo que añadimos el fumé, Añadir el pescado en trozos de un bocado y las almejas, Apagar la vitrocerámica, tapar y dejar reposar 3-4min, Exprimir media lima por todo el arroz, Servir'],
+  },
+];
