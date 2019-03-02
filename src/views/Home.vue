e11c0629527e92e75fc4cd090649f89918cb9824
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-03-02 12:37:13 +0100

    Removed unnecesary HelloWorld Component and imported Sass scaffolding globally

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/img/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/img/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import Recipes from '@/assets/db/recipes.js';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
   },
 };
 </script>
