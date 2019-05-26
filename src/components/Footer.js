d446f6246e2fcf7cd88035991ba5eed0d18bc867
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-05-26 21:19:58 +0200

    Removed <link> from react-reouter-dom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Footer() {
     return (
         <footer className="foo" role="contentinfo">
             <span className="u-nowrap">You can see my&nbsp;</span>
-            <Link className="foo__link" to={`https://ignaciodenuevo.com/`} target="_blank" rel="noopener noreferrer">
+            <a className="foo__link" href="https://ignaciodenuevo.com/" target="_blank" rel="noopener noreferrer">
                 Website
-            </Link>
+            </a>
             <span className="u-nowrap">&nbsp;or follow me on&nbsp;</span>
-            <Link className="foo__link" to={`https://github.com/IgnaciodeNuevo`} target="_blank" rel="noopener noreferrer">
+            <a className="foo__link" href="https://github.com/IgnaciodeNuevo" target="_blank" rel="noopener noreferrer">
                 GitHub
-            </Link>
+            </a>
             <span className="u-nowrap">&nbsp;or&nbsp;</span>
-            <Link className="foo__link" to={`https://twitter.com/IgnaciodeNuevo`} target="_blank" rel="noopener noreferrer">
+            <a className="foo__link" href="https://twitter.com/IgnaciodeNuevo" target="_blank" rel="noopener noreferrer">
                 Twitter
-            </Link>
+            </a>
         </footer>
     )
 }
