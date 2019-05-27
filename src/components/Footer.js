a592ebab9d16c05627f8d79799dd17d43f10a595
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-05-27 12:26:22 +0200

    Updated footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="foo" role="contentinfo">
-            <span className="u-nowrap">You can see my&nbsp;</span>
-            <a className="foo__link" href="https://ignaciodenuevo.com/" target="_blank" rel="noopener noreferrer">
-                Website
-            </a>
-            <span className="u-nowrap">&nbsp;or follow me on&nbsp;</span>
-            <a className="foo__link" href="https://github.com/IgnaciodeNuevo" target="_blank" rel="noopener noreferrer">
-                GitHub
-            </a>
-            <span className="u-nowrap">&nbsp;or&nbsp;</span>
-            <a className="foo__link" href="https://twitter.com/IgnaciodeNuevo" target="_blank" rel="noopener noreferrer">
-                Twitter
-            </a>
+
         </footer>
     )
 }
