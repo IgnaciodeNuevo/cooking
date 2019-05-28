404074e7a6f2ecee07f10001cc7a3a47d1b96dc9
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2019-05-28 10:57:34 +0200

    searchAdded new routes: search, favorites & about

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import { HomeContainer } from './containers/index'
+import { HomeContainer, FavoritesContainer, SearchContainer, ShoppingCardContainer } from './containers/index'
 import { Header, About, Footer } from './components/index';
 
 class App extends Component {
@@ -10,6 +10,9 @@ class App extends Component {
             <Router>
                 <Header />
                 <Route path="/" exact component={HomeContainer} />
+                <Route path="/search" component={SearchContainer} />
+                <Route path="/favorites" component={FavoritesContainer} />
+                <Route path="/cart" component={ShoppingCardContainer} />
                 <Route path="/about" component={About} />
                 <Footer />
             </Router>
