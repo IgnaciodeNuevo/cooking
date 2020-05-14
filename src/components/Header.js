6dba3cf904b731d8813f11e932008088bb7f435d
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2020-05-14 17:51:12 +0200

    Update web to Gastby v2.21.22

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
 
-import { ReactComponent as Logo } from '../assets/icons/logo.svg';
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
 
-
-function Header() {
-    return (
-        <header className="header" role="banner">
-            <Link className="header__link" to={`/`}>
-                <Logo className="header__img" />
-                <h1 className="playfair">Cooking</h1>
-            </Link>
-        </header>
-    )
+Header.propTypes = {
+  siteTitle: PropTypes.string,
 }
 
-export default Header;
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
