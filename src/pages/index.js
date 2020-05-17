fcdd12f2ca817a107d22f5b584a8a36051feaa0e
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2020-05-17 20:23:40 +0200

    Removed unnecesary style

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,15 +31,6 @@ const StyledLink = styled(props => <Link {...props} />)`
     }
 `;
 
-// grid-template-columns
-//  uses 320px (which is the minimum devide width the web supports)
-//  minus --space-l * 2 used as padding-left and padding-right
-const Grid = styled.section`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(calc(320px - (var(--space-l) * 2)), 1fr));
-    grid-gap: var(--space-l);
-`
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Recetas" />
