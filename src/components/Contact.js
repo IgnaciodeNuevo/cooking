0ae7f77a66840d46d9907e3741dc8140264f8571
IgnaciodeNuevo <ignaciodenuevo@posteo.net>
2020-05-20 21:32:43 +0200

    Refactor Footer and add Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,83 @@
+import React from "react"
+import styled from "styled-components"
+
+const Form = styled.form`
+  background-color: var(--color-brand-tertiary-medium);
+  padding: var(--space-l);
+  border-radius: var(--border-radius);
+`
+
+const Wrapper = styled.div`
+  margin-bottom: var(--space-l);
+`
+
+const Label = styled.label`
+  font-family: var(--font-family-heading);
+  font-size: var(--h6);
+  display: block;
+  margin-bottom: var(--space-s);
+`
+
+const Input = styled.input`
+  font-size: var(--h6);
+  display: block;
+  padding: var(--space-s);
+  border: 3px solid var(--color-brand-primary-dark);
+  border-radius: var(--border-radius);
+  color: var(--color-brand-primary-dark);
+`
+
+const Textarea = styled.textarea`
+  font-size: var(--h6);
+  display: block;
+  margin-bottom: var(--space-s);
+  padding: var(--space-s);
+  border: 3px solid var(--color-brand-primary-dark);
+  border-radius: var(--border-radius);
+  color: var(--color-brand-primary-dark);
+`
+
+const Button = styled.button`
+  background-color: var(--color-brand-primary-dark);
+  border: 0;
+  padding: var(--space-m);
+  border-radius: var(--border-radius);
+  font-size: var(--font-base);
+  color: var(--color-base-lightest);
+  transition: all var(--transition-duration-normal)
+    var(--animation-type-cubic-fast);
+
+  &:hover {
+    background-color: var(--color-brand-primary-medium);
+    cursor: pointer;
+  }
+`
+
+const H2 = styled.h2`
+  margin-bottom: var(--space-m);
+  color: var(--color-base-lightest);
+`
+
+const Contact = () => (
+    <Form name="contact" method="POST" data-netlify="true">
+        <H2>Contacto</H2>
+        <Wrapper>
+            <Label htmlFor="name">Tu nombre:</Label>
+            <Input name="name" type="text" placeholder="D. Joe" />
+        </Wrapper>
+
+        <Wrapper>
+            <Label htmlFor="email">Tu email:</Label>
+            <Input name="email" type="email" placeholder="d.joe@email.com" />
+        </Wrapper>
+
+        <Wrapper>
+            <Label htmlFor="message">Mesaje:</Label>
+            <Textarea name="message" placeholder="Mi mensaje es..." />
+        </Wrapper>
+
+        <Button type="submit">Enviar</Button>
+    </Form>
+)
+
+export default Contact
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,16 @@ import React from "react"
 import styled from "styled-components"
 
 import Menu from "./menu"
+import Contact from "./contact"
 
 const PageFooter = styled.footer`
   background: var(--color-brand-primary-dark);
   color: var(--color-base-lightest);
+  padding: var(--space-l);
+
+  @media (min-width: 80em) {
+    display: flex;
+  }
 `
 
 const Paragraph = styled.p`
@@ -21,35 +27,65 @@ const MenuWrapper = styled.div`
   margin: 0 auto;
   max-width: var(--component-max-with);
   padding: var(--space-xl) var(--space-l) var(--space-l);
+
+  @media (min-width: 80em) {
+    padding-top: 0;
+    padding-right: 0;
+    padding-bottom: var(--space-l);
+    padding-left: var(--space-xl);
+  }
 `
 
 const InfoWrapper = styled.div`
   margin: 0 auto;
   max-width: var(--component-max-with);
-  padding: var(--space-s) var(--space-l) var(--space-xl);
+  padding: 0 var(--space-l) var(--space-xl);
+
+  @media (min-width: 80em) {
+      padding-top: 0;
+      padding-right: 0;
+      padding-bottom: 0;
+      padding-left: var(--space-xl);
+  }
+`
+
+const H2 = styled.h2`
+  margin-bottom: 0;
+  color: var(--color-base-lightest);
+  padding-top: var(--space-xl);
+  padding-left: var(--space-l);
+
+  @media (min-width: 80em) {
+    padding-top: var(--space-l);
+    padding-left: var(--space-xl);
+  }
 `
 
 const Footer = ({ author, twitter, github, web }) => (
   <PageFooter>
-    <MenuWrapper>
-      <Menu />
-    </MenuWrapper>
-    <InfoWrapper>
-      <Paragraph>
-        ©{new Date().getFullYear()}. Creado por {author}
-      </Paragraph>
-      <Paragraph>
-        Sígueme en{" "}
-        <Link href={twitter} target="_blank" rel="noopener noreferrer me">
-          Twitter
-        </Link>
-        ,{" "}
-        <Link href={github} target="_blank" rel="noopener noreferrer me">
-          GitHub
-        </Link>{" "}
-        o échale un vistazo a <Link href={web}>web</Link>.
-      </Paragraph>
-    </InfoWrapper>
+    <Contact />
+    <div>
+        <H2>Info</H2>
+        <MenuWrapper>
+            <Menu />
+        </MenuWrapper>
+        <InfoWrapper>
+            <Paragraph>
+                ©{new Date().getFullYear()}. Creado por {author}
+            </Paragraph>
+            <Paragraph>
+                Sígueme en{" "}
+                <Link href={twitter} target="_blank" rel="noopener noreferrer me">
+                Twitter
+                </Link>
+                ,{" "}
+                <Link href={github} target="_blank" rel="noopener noreferrer me">
+                GitHub
+                </Link>{" "}
+                o échale un vistazo a <Link href={web}>web</Link>.
+            </Paragraph>
+        </InfoWrapper>
+    </div>
   </PageFooter>
 )
 
